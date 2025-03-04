5d575e4be3c782a46adb0c734b30cb0fcc698d09
Evan <evangrevenn@gmail.com>
2025-03-04 09:19:58 +0100

    env

--- db.client.js
+++ db.client.js
@@ -1,23 +1,24 @@
 const { Sequelize } = require('sequelize')
+require('dotenv').config();
 
 // database
 const sequelize = new Sequelize(
-  'postgresql://esgi_cloud_exam_43189_user:4A6VZAN3RUOuxQrgR68ImTIT0at1zuQA@dpg-cv3b3qd2ng1s73ftd7jg-a/esgi_cloud_exam_43189', // TODO
-  {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+    process.env.DATABASE_URL,
+    {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
     },
-  },
 );
 
 // authentication and synchronization
 sequelize.authenticate()
-  .then(() => {
-    sequelize.sync().catch(() => console.log("Cannot sync the database"));
-  })
-  .catch(() => console.log("Cannot connect to database, please check environment credentials"));
+    .then(() => {
+        sequelize.sync().catch(() => console.log("Cannot sync the database"));
+    })
+    .catch(() => console.log("Cannot connect to database, please check environment credentials"));
 
 module.exports = sequelize;
